7afd9082499a27189672085ee23af039f49b2fea
Anthony Rivas Chire <arivasc@unsa.edu.pe>
2025-05-22 11:07:39 -0500

    lab 02 archivos de unity

---
+++ lab02/Move.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Move : MonoBehaviour {
+
+    public Vector3 goal = new Vector3(5, 0, 4);
+    public float speed = 1f;
+
+    void Start() {
+        
+    }
+
+    void LateUpdate() {
+        this.transform.Translate(goal.normalized * speed * Time.deltaTime);
+
+    }
+}
---
+++ lab02/MoveToGoal.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MoveToGoal : MonoBehaviour {
+
+    public float speed = 2.0f;
+    public Transform goal;
+
+    void Start() {
+        
+    }
+
+    void LateUpdate() {
+        Vector3 direction = goal.position - this.transform.position;
+        this.transform.Translate(direction.normalized * speed * Time.deltaTime);
+
+    }
+}
