db9ca533551029006d3e375e31ce625e71c93e31
Anthony Rivas <arivasc@unsa.edu.pe>
2025-05-29 02:11:53 -0500

    lab 04 done

---
+++ lab04/Assets/Scripts/Cup.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class Cup : MonoBehaviour
+{
+    // Contador individual por cada Cup
+    public int score = 0;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger entered with: " + collision.gameObject.name);
+        // Verifica si lo que entró es una pelota
+        Ball ball = collision.GetComponent<Ball>();
+        if (ball != null)
+        {
+            score++;
+            Debug.Log("Ball entered cup! Total: " + score);
+
+            // O actualizar un contador global
+            GameManager.Instance.AddScore();
+        }
+    }
+	
+}
